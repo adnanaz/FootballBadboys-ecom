0c26fe0715835751294af2e8bc446da3ccc14376
Arfian Dimas Andi Permana <arfiandimas1929@gmail.com>
2020-05-10 16:13:00 +0700

    Javascript

--- master/resources/views/customer/index.blade.php
+++ master/resources/views/customer/index.blade.php
@@ -160,4 +160,60 @@
 
     </section>
 
+    <script>
+        var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        // init: false,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        speed: 900,
+        loop: true,
+        autoplay: {
+            delay: 1500,
+
+            disableOnInteraction: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1400: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            1600: {
+                slidesPerView: 5,
+                spaceBetween: 20,
+            },
+        }
+    });
+    </script>
+
 @endsection
--- master/resources/views/layout/customer.blade.php
+++ master/resources/views/layout/customer.blade.php
@@ -372,61 +372,6 @@
 
 <script src="/js/main.js"></script>
 <script>
-
-    var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        // init: false,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        speed: 900,
-        loop: true,
-        autoplay: {
-            delay: 1500,
-
-            disableOnInteraction: true,
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-            },
-            640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-
-            992: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-            },
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-            },
-            1400: {
-                slidesPerView: 4,
-                spaceBetween: 20,
-            },
-            1600: {
-                slidesPerView: 5,
-                spaceBetween: 20,
-            },
-        }
-    });
-
     $("#checkAll").click(function () {
         $(".check").prop('checked', $(this).prop('checked'));
     });
@@ -443,29 +388,6 @@
             container.find('.search-input').val('');
         }
     }
-
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-        spaceBetween: 10,
-        slidesPerView: 5,
-        speed: 900,
-        freeMode: false,
-        loopedSlides: 1, //looped slides should be the same
-
-    });
-    var galleryTop = new Swiper('.gallery-top', {
-        spaceBetween: 10,
-        slidesPerView: 1,
-        freeMode: false,
-        speed: 900,
-        loopedSlides: 1, //looped slides should be the same
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        thumbs: {
-            swiper: galleryThumbs,
-        },
-    });
 </script>
 
 </html>
